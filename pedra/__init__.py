0041d0b674de36d3fa44a3d35c72dc214402891a
depra <mariodepra@ucf.edu>
2024-07-11 15:09:45 -0500

    adding combine func

--- pedra/__init__.py
+++ pedra/__init__.py
@@ -3,7 +3,7 @@ r'''
 
 from .core import Image, loadimage, loadimage_batch, loadmosaic, check_fits_structure
 from .viewer import CardinalViewer, ImageViewer
-from .reduc import biascorrect, flatcorrect
+from .reduc import biascorrect, flatcorrect, combine
 from .util import smallbody_ephem
 from .apps import ContrastViewer, ImageListViewer
 from .photometry import circular_photometry, calculate_sky_background

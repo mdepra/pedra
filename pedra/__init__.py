ac512832b221b7b464d6f641f6a55197e371998a
depra <mariodepra@ucf.edu>
2024-07-11 15:12:18 -0500

    fixing minor

--- pedra/__init__.py
+++ pedra/__init__.py
@@ -6,4 +6,4 @@ from .viewer import CardinalViewer, ImageViewer
 from .reduc import biascorrect, flatcorrect, combine
 from .util import smallbody_ephem
 from .apps import ContrastViewer, ImageListViewer
-from .photometry import circular_photometry, calculate_sky_background
+from .phot import circular_photometry, calculate_sky_background
